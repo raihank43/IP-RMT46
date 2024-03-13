5b606cb69a8384ce5294da4ef2e12beb7a75945e
raihank43 <raihankusuma5@gmail.com>
2024-03-13 15:23:51 +0700

    feat: add first separate routers

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const HomeController = require("./controllers/HomeController");
 const app = express();
 const port = 3000;
+const router = require("./routes");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get("/", HomeController.Home);
+app.use(router);
 
 app.listen(port, () => {
   console.log("app running on port 3000");
