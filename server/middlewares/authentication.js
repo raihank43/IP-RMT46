478e6d324ff6501e50aa140725f68c1fc9eda1f4
raihank43 <raihankusuma5@gmail.com>
2024-03-13 16:32:49 +0700

    feat: add authentication

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,33 @@
+const { verifyToken } = require("../helpers/jwt");
+const { User } = require("../models");
+async function authentication(req, res, next) {
+  try {
+    let access_token = req.headers.authorization;
+
+    if (!access_token) {
+      throw { name: "Unauthenticated" };
+    }
+
+    let [type, token] = access_token.split(" ");
+    if (type !== "Bearer") {
+      throw { name: "Unauthenticated" };
+    }
+
+    const payload = verifyToken(token);
+    let user = await User.findByPk(payload.id);
+    if (!user) {
+      throw { name: "Unauthenticated" };
+    }
+
+    req.user = {
+      id: user.id,
+      email: user.email,
+    };
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+
+module.exports = authentication;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const HomeController = require("../controllers/HomeController");
 const UserController = require("../controllers/UserController");
+const authentication = require("../middlewares/authentication");
 const router = express.Router();
 
 router.post("/register", UserController.Register);
-router.post("/login", UserController.Login)
+router.post("/login", UserController.Login);
+
+router.use(authentication);
 router.get("/", HomeController.Home);
 
 module.exports = router;
