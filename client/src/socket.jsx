4a1bba3497d140a4ac73d682948f6aa833b20809
raihank43 <raihankusuma5@gmail.com>
2024-06-06 13:43:46 +0700

    feat: change server path

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://koneksion.raihankusuma.tech/"
+    ? "https://koneksion.raihandeploy.my.id/"
     : "http://localhost:3000";
 
 export const socket = io(URL, {
